1d5ef8fd12b7599969c3de7a468588a54942fa43
CarolLu85 <little_shrewdness@hotmail.com>
2024-05-27 11:51:19 +0300

    update proj1

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,4 +1,73 @@
 package deque;
 
-public class MaxArrayDeque {
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    private Comparator<T> comp;
+    public MaxArrayDeque(Comparator<T> c) {
+        super();
+        this.comp =c;
+
+    }
+
+    /*returns the maximum element in the deque as governed by the previously given Comparator.
+     */
+    public T max(){
+        if (this.isEmpty()){return null;}
+        T maxElement = this.get(0);
+        for (T element: this){
+            if (comp.compare(element, maxElement) > 0){
+                maxElement = element;
+            }
+        }
+        return maxElement;
+    }
+
+    /* returns the maximum element in the deque as governed by the parameter Comparator c.
+    */
+    public T max(Comparator<T> c){
+        if (this.isEmpty()){return null;}
+        T maxElement = this.get(0);
+        for (T element: this){
+            if (c.compare(element, maxElement) > 0){
+                maxElement = element;
+            }
+        }
+        return maxElement;
+    }
+    public static void main(String[] args){
+//        Comparator<Integer> defaultComparator = new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer a, Integer b) {
+//                return a.compareTo(b);
+//            }
+//        };
+
+//        Comparator<Integer> defaultComparator = (a, b) -> a.compareTo(b);
+
+
+        Comparator<Integer> defaultComparator =  Integer::compareTo;
+        MaxArrayDeque<Integer> maxDeque = new MaxArrayDeque<>(defaultComparator);
+
+        maxDeque.addFirst(1);
+        maxDeque.addFirst(2);
+        maxDeque.addFirst(3);
+
+        System.out.println("the max is: " + maxDeque.max());
+
+        Comparator<String> stringComparator =  String::compareTo;
+        MaxArrayDeque<String> maxStringDeque = new MaxArrayDeque<>(stringComparator);
+
+        maxStringDeque.addLast("apple");
+        maxStringDeque.addLast("banana");
+        maxStringDeque.addLast("cherry");
+
+        System.out.println("Max string (natural order): " + maxStringDeque.max(stringComparator));
+
+        //self-defined comparator
+        Comparator<String> lengthComparator = (a, b) -> Integer.compare(a.length(), b.length());
+        System.out.println("Max string (by length): " + maxStringDeque.max(lengthComparator));
+
+    }
 }
+
--- proj1/deque/MaxArrayDequeTest.java
+++ proj1/deque/MaxArrayDequeTest.java
@@ -1,4 +1,154 @@
 package deque;
 
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.junit.Assert.*;
+
+
+/** Performs some basic linked list tests. */
 public class MaxArrayDequeTest {
+    @Test
+    /** Adds a few things to the list, checking isEmpty() and size() are correct,
+     * finally printing the results.
+     *
+     * && is the "and" operation. */
+    public void addIsEmptySizeTest() {
+        Comparator<String> cmp = new Comparator<>() {
+
+            @Override
+            public int compare(String strA, String strB) {
+                return strB.compareTo(strA);
+            }
+        };
+
+        // System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+
+        MaxArrayDeque<String> ad1 = new MaxArrayDeque<String>(cmp);
+
+        assertTrue("A newly initialized LLDeque should be empty", ad1.isEmpty());
+        ad1.addFirst("front");
+
+        // The && operator is the same as "and" in Python.
+        // It's a binary operator that returns true if both arguments true, and false otherwise.
+        assertEquals(1, ad1.size());
+        assertFalse("ad1 should now contain 1 item", ad1.isEmpty());
+
+        ad1.addLast("middle");
+        assertEquals(2, ad1.size());
+
+        ad1.addLast("back");
+        assertEquals(3, ad1.size());
+
+        System.out.println("Printing out deque: ");
+        ad1.printDeque();
+    }
+
+    @Test
+    /** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
+    public void addRemoveTest() {
+        Comparator<Integer> cmp = new Comparator<>() {
+
+            @Override
+            public int compare(Integer strA, Integer strB) {
+                return strB.compareTo(strA);
+            }
+        };
+
+        // System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+
+        MaxArrayDeque<Integer> ad1 = new MaxArrayDeque<Integer>(cmp);
+        // should be empty
+        assertTrue("ad1 should be empty upon initialization", ad1.isEmpty());
+
+        ad1.addFirst(10);
+        // should not be empty
+        assertFalse("ad1 should contain 1 item", ad1.isEmpty());
+
+        ad1.removeFirst();
+        // should be empty
+        assertTrue("ad1 should be empty after removal", ad1.isEmpty());
+
+    }
+
+    @Test
+    /* Tests removing from an empty deque */
+    public void removeEmptyTest() {
+        Comparator<Integer> cmp = new Comparator<>() {
+
+            @Override
+            public int compare(Integer strA, Integer strB) {
+                return strB.compareTo(strA);
+            }
+        };
+
+        // System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+
+        MaxArrayDeque<Integer> ad1 = new MaxArrayDeque<>(cmp);
+        ad1.addFirst(3);
+
+        ad1.removeLast();
+        ad1.removeFirst();
+        ad1.removeLast();
+        ad1.removeFirst();
+
+        int size = ad1.size();
+        String errorMsg = "  Bad size returned when removing from empty deque.\n";
+        errorMsg += "  student size() returned " + size + "\n";
+        errorMsg += "  actual size() returned 0\n";
+
+        assertEquals(errorMsg, 0, size);
+    }
+
+
+    @Test
+    /* check if null is return when removing from an empty MaxArrayDeque. */
+    public void emptyNullReturnTest() {
+        Comparator<Integer> cmp = new Comparator<>() {
+
+            @Override
+            public int compare(Integer strA, Integer strB) {
+                return strB.compareTo(strA);
+            }
+        };
+        // System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+
+        MaxArrayDeque<Integer> ad1 = new MaxArrayDeque<Integer>(cmp);
+
+        boolean passed1 = false;
+        boolean passed2 = false;
+        assertEquals("Should return null when removeFirst is called on an empty Deque,", null, ad1.removeFirst());
+        assertEquals("Should return null when removeLast is called on an empty Deque,", null, ad1.removeLast());
+
+
+    }
+
+    @Test
+    /* Add large number of elements to deque; check if order is correct. */
+    public void bigLLDequeTest() {
+        Comparator<Integer> cmp = new Comparator<>() {
+
+            @Override
+            public int compare(Integer strA, Integer strB) {
+                return strB.compareTo(strA);
+            }
+        };
+        // System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+
+        MaxArrayDeque<Integer> ad1 = new MaxArrayDeque<Integer>(cmp);
+        for (int i = 0; i < 1000000; i++) {
+            ad1.addLast(i);
+        }
+
+        for (double i = 0; i < 500000; i++) {
+            assertEquals("Should have the same value", i, (double) ad1.removeFirst(), 0.0);
+        }
+
+        for (double i = 999999; i > 500000; i--) {
+            assertEquals("Should have the same value", i, (double) ad1.removeLast(), 0.0);
+        }
+
+
+    }
 }
